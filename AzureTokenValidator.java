bf624c876ef73e57126c671e1b5aa4e7a1644601
Ajinkya-Ronghe <82195189+Ajinkya-Ronghe@users.noreply.github.com>
2023-11-03 15:42:49 +0530

    Update AzureTokenValidator.java

--- AzureTokenValidator.java
+++ AzureTokenValidator.java
@@ -1,12 +1,11 @@
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 
 import java.net.URL;
-import java.security.Key;
-import java.security.interfaces.RSAPublicKey;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Map;
 
@@ -20,16 +19,16 @@ public class AzureTokenValidator {
         // Fetch JWKS (JSON Web Key Set)
         URL url = new URL(jwksUri);
         Map<String, Object> jwkSet = Jwts.parserBuilder().build().parseClaimsJws(url.openStream().toString()).getBody();
-        
-        // Extract public key from JWKS using the kid (Key ID) from the token
+
+        // Extract the public key from JWKS using the kid (Key ID) from the token
         String kid = Jwts.parserBuilder().build().parseClaimsJws(azureToken).getHeader().getKeyId();
         Map<String, Object> keyData = (Map<String, Object>) jwkSet.get(kid);
         String n = (String) keyData.get("n");
         String e = (String) keyData.get("e");
-        RSAPublicKey publicKey = (RSAPublicKey) Keys.rsa(
-            Base64.getUrlDecoder().decode(n),
-            Base64.getUrlDecoder().decode(e)
-        ).getPublic();
+        
+        byte[] modulusBytes = Base64.getUrlDecoder().decode(n);
+        byte[] exponentBytes = Base64.getUrlDecoder().decode(e);
+        PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(modulusBytes));
 
         // Parse and validate the Azure token
         Jws<Claims> jwsClaims = Jwts.parserBuilder()
